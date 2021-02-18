11d9e3184ec80c2aa7d0ea3227903f0a3206a5a6
MatrixStore <matrixstore@email.cz>
2021-02-18 22:58:46 +0100

    Add Paging.X.PagedList

---
+++ src/Data.Paging.X.PagedList/SimplePagedList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using X.PagedList;
+
+namespace Olbrasoft.Data.Paging.X.PagedList
+{
+    public class SimplePagedList<T> : BasePagedList<T>
+    {
+        public SimplePagedList(IEnumerable<T> subSet, int pageNumber, int pageSize, int totalItemCount) : base(pageNumber, pageSize, totalItemCount)
+        {
+            if (subSet == null)
+                throw new ArgumentNullException(nameof(subSet));
+
+            Subset.AddRange(subSet);
+        }
+    }
+}
